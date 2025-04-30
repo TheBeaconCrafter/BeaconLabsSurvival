3f6f2296bc627ff15ea3d88179a0eff1085b0009
Vincent Wackler <vincent@bcnlab.org>
2025-04-30 10:33:08 +0200

    Initial commit

--- src/main/java/org/bcnlab/beaconLabsSurvival/BeaconLabsSurvival.java
+++ src/main/java/org/bcnlab/beaconLabsSurvival/BeaconLabsSurvival.java
@@ -1,17 +1,56 @@
 package org.bcnlab.beaconLabsSurvival;
 
+import org.bcnlab.beaconLabsSurvival.command.LabsSurvivalCommand;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BeaconLabsSurvival extends JavaPlugin {
 
+    private String pluginPrefix;
+    private String pluginVersion = "1.0";
+    private String noPermsMessage = "&cYou do not have permission to use this command.";
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // Config
+        createDefaultConfig();
+        loadConfig();
 
+        // Commands
+        getCommand("labssurvival").setExecutor(new LabsSurvivalCommand(this));
+
+        // Finished
+        getLogger().info("BeaconLabsSurvival has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Finished
+        getLogger().info("BeaconLabsSurvival has been disabled!");
+    }
+
+    public String getPrefix() {
+        return ChatColor.translateAlternateColorCodes('&', pluginPrefix);
+    }
+
+    public String getVersion() {
+        return pluginVersion;
+    }
+
+    public String getNoPermsMessage() {
+        return ChatColor.translateAlternateColorCodes('&', noPermsMessage);
+    }
+
+    private void loadConfig() {
+        FileConfiguration config = getConfig();
+        pluginPrefix = config.getString("plugin-prefix", "&6BeaconLabs &8» ");
+    }
+
+    private void createDefaultConfig() {
+        FileConfiguration config = getConfig();
+        config.options().copyDefaults(true);
+        config.addDefault("plugin-prefix", "&6BeaconLabs &8» ");
+        saveConfig();
     }
 }
--- src/main/java/org/bcnlab/beaconLabsSurvival/command/LabsSurvivalCommand.java
+++ src/main/java/org/bcnlab/beaconLabsSurvival/command/LabsSurvivalCommand.java
@@ -1,4 +1,22 @@
 package org.bcnlab.beaconLabsSurvival.command;
 
-public class LabsSurvivalCommand {
+import org.bcnlab.beaconLabsSurvival.BeaconLabsSurvival;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class LabsSurvivalCommand implements CommandExecutor {
+
+    private final BeaconLabsSurvival plugin;
+
+    public LabsSurvivalCommand(BeaconLabsSurvival plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        sender.sendMessage(plugin.getPrefix() + ChatColor.RED + "BeaconLabsSurvival Version " + ChatColor.GOLD + plugin.getVersion() + ChatColor.RED + " by ItsBeacon");
+        return true;
+    }
 }
